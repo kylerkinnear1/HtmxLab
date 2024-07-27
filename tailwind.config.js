c453053a9c9d353ef9a1386123da7d217e3f3285
Kyle Kinnear <kylerkinnear@gmail.com>
2024-07-27 17:19:30 -0500

    Added tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+	"./HtmxLab/Templates/*.{html,liquid}",
+	"./HtmxLab/wwwroot/*.{html,liquid}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
